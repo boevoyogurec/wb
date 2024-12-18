a95d3481cc9840d80d47f91f925eb938ad918532
boevoyogurec <pvoit2006@gmail.com>
2024-12-18 13:19:01 +0300

    первый код

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-#main стартовый модуль
+import pandas as pd
+import re
 
+# Функция для очистки номера телефона
+def clean_phone_number(phone):
+    # Удаляем все символы, кроме цифр
+    cleaned = re.sub(r'\D', '', phone)
+    return cleaned
+
+# Чтение файла Excel
+input_file = 'phone_numbers.xlsx'
+df = pd.read_excel(input_file, engine='openpyxl')
+
+# Предполагаем, что номера телефонов находятся в столбце с именем 'Телефон'
+df['phone_number'] = df['phone_number'].apply(clean_phone_number)
+
+# Сохранение результата в новый файл Excel
+output_file = 'cleaned_phone_numbers.xlsx'
+df.to_excel(output_file, index=False, engine='openpyxl')
